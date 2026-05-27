79cd2e5035914986f695fb9cee5e87adfbd2f278
Gus Satria <satria070906@gmail.com>
2026-05-27 22:51:38 +0700

    pertemuan 12

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,36 +1,36 @@
 @extends('template')
 @section('title', 'Data Pegawai')
 @section('konten')
-	<a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Pegawai Baru</a>
+    <a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Pegawai Baru</a>
+    <br />
+    <br />
+    <p>Cari Data Pegawai :</p>
+    <form action="/pegawai" method="GET">
+        <input type="text" name="cari" placeholder="Cari Pegawai .." class="form-control">
+        <input type="submit" value="CARI">
+    </form>
+    <br />
+    <table class="table table-striped table-hover ">
+        <tr>
+            <th>Nama</th>
+            <th>Jabatan</th>
+            <th>Umur</th>
+            <th>Alamat</th>
+            <th>Opsi</th>
+        </tr>
+        @foreach ($pegawai as $p)
+            <tr>
+                <td>{{ $p->pegawai_nama }}</td>
+                <td>{{ $p->pegawai_jabatan }}</td>
+                <td>{{ $p->pegawai_umur }}</td>
+                <td>{{ $p->pegawai_alamat }}</td>
+                <td>
+                    <a href="/pegawai/edit/{{ $p->pegawai_id }}"class="btn btn-warning">Edit</a>
 
-	<br/>
-	<br/>
-
-	<br/>
-	<br/>
-	<p>Cari Data Pegawai :</p>
-	<form action="/pegawai/cari" method="GET" class="form-inline">
-        <div class="form-group">
-		<input type="text" name="cari" placeholder="Cari Pegawai .."  class="form-control">
-		<input type="submit" value="CARI" class="btn btn-dark mt-2">
-        </div>
-	</form>
-	<br/>
-	<br/>
-
-    <table class="table table-striped table-hover">
-		<tr>
-			<th>Nama</th>
-			<th>Jabatan</th>
-            <td>{{ $p->pegawai_umur }}</td>
-			<td>{{ $p->pegawai_alamat }}</td>
-			<td>
-                <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning btn-sm">Edit</a>
-
-				<a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger btn-sm">Hapus</a>
-			</td>
-		</tr>
-		@endforeach
-	</table>
-     {{ $pegawai->links() }} <!-- untuk menampilkan pagination -->
-	@endsection
+                    <a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    {{ $pegawai->links() }}
+@endsection
